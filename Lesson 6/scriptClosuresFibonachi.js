8db17e5453e6a1cf2c69b4196f70305522057328
SergeyGoncharenko <mailgoncharenko@gmail.com>
2015-12-14 12:41:19 +0200

    JavaScript
    Массивы, замыкания, область видимости, Числа Фибоначи, WebWorkers

--- Lesson 6/scriptClosuresFibonachi.js
+++ Lesson 6/scriptClosuresFibonachi.js
@@ -1,21 +1,51 @@
-﻿function calcFib(x) {
-    if (x > 1) {
-        return calcFib(x - 1) + calcFib(x - 2);
-    }
-    else {
-        return x;
+﻿//version____________________________1___(use webWorker)
+window.onload = function () {
+    document.getElementById("startButton").onclick = function () {
+        // Создание нового потока
+        var worker = new Worker("fibonachiWorker.js");
+
+        // Создание обработчика для получения сообщений от потока
+        worker.addEventListener("message", function (e) {
+            document.getElementById("output").innerHTML += e.data + "<br />";
+        }, false);
+
+        // запуск потока
+        worker.postMessage("");
     }
 }
-
-addEventListener("message", function () {
-
-    for (var i = 0; i < 50; i++) {
-        postMessage(i + " = " + calcFib(i) + "<br />");
-    }
-
-}, false);
 
 //3. Реализуйте функцию вычисления ряда чисел Фибоначчи, используя замыкания-->
 //Чи́сла Фибона́ччи — элементы числовой последовательности
 //0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, …
 //в которой каждое последующее число равно сумме двух предыдущих чисел.
+
+//version____________________________2
+
+// честно стырил
+//    function fib(n) {
+//        return function (n, a, b) {
+//            return n > 0 ? arguments.callee(n - 1, b, a + b) : a;
+//        }(n, 0, 1);
+//    }
+
+
+//version____________________________3
+
+//var fib1 = function () {
+//    var a = 1, b = 1, c = 0;
+//    return function (n) {
+//        if (n == 1) return a;
+//        for (var i = 2; i < n; i++) {
+//            c = a + b;
+//            a = b;
+//            b = c;
+//        }
+//        return b;
+//    }
+//}
+//var fib2 = function (n) {
+//    return n > 1 ? fib2(n - 1) + fib2(n - 2) : n;
+//};
+//var numb = fib1();
+//console.log(numb(9));
+//console.log(fib2(9));
--- Lesson 6/scriptCompareArrays.js
+++ Lesson 6/scriptCompareArrays.js
@@ -1,4 +1,6 @@
-﻿var a = [8, 3, 5];
+﻿//version____________________________1
+
+var a = [8, 3, 5];
 var b = [5, 1, 4];
 
 function compare(arr1, arr2) {
@@ -10,7 +12,7 @@ function compare(arr1, arr2) {
 
     // array comparison
     function isEqual(arr1, arr2) {
-        return  ((arr1.sort().toString() === arr2.sort().toString()) && (arr1.length == arr2.length)) 
+        return ((arr1.sort().toString() === arr2.sort().toString()) && (arr1.length == arr2.length))
     }
     console.log('Проверка на равенство длины массивов и равенство элементов: ' + isEqual(arr1, arr2));
 
@@ -26,6 +28,7 @@ function compare(arr1, arr2) {
 
 
     function bigger() {
+        var bigger;
         if (sum1 > sum2) {
             ((bigger = arr1.toString(), result = true), true);
         }
@@ -41,15 +44,9 @@ function compare(arr1, arr2) {
     }
 }
 console.log(compare());
-var a = [7, 2];
-var b = [7, 2];
-console.log('compare([7,2], [7,2]) ' + bigger([7, 2], [7, 2]));
-var a = [3, 1, 4];
-var b = [8];
-console.log('compare([3,1,4], [8]) ' + bigger([3, 1, 4], [8]));
-var a = [3, 1, 4];
-var b = [8, 2];
-console.log('compare([3,1,4], [8,2]) ' + bigger([3, 1, 4], [8, 2]));
+//console.log('compare([7,2], [7,2]) ' + bigger([7, 2], [7, 2]));
+//console.log('compare([3,1,4], [8]) ' + bigger([3, 1, 4], [8]));
+//console.log('compare([3,1,4], [8,2]) ' + bigger([3, 1, 4], [8, 2]));
 
 // 2. Написать функцию, которая сравнивает 2 числовых массива (А, В)
 // результатом выполнения функции должно быть:
@@ -62,3 +59,79 @@ console.log('compare([3,1,4], [8,2]) ' + bigger([3, 1, 4], [8, 2]));
 // compare([3,1,4], [8]) -> {result:false, bigger:[]}
 // compare([3,1,4], [8,2]) -> {result:false, bigger:[2,8]}
 
+
+
+
+//version____________________________2
+
+//var compare = function (arr1, arr2) {
+//    if (arr1.length == arr2.length) {
+//        for (var i = 0; i < arr1.length; i++) {
+//            if (arr1[i] == arr2[i]) {
+//                return true;
+//            }
+//        }
+//    } else {
+//        var bigger = [];
+//        var result = false;
+//        var sortArr = function (a, b) {
+//            return a - b;
+//        };
+//        var sum1 = arr1.reduce(function (a, b) {
+//            return a + b;
+//        });
+//        var sum2 = arr2.reduce(function (a, b) {
+//            return a + b;
+//        });
+//        if (sum1 == sum2) {
+//            console.log("result:" + result + ", bigger:", bigger);
+//        } else if (sum1 > sum2) {
+//            console.log("result:" + result + ",", arr1.sort(sortArr));
+//        }
+//        else {
+//            console.log("result:" + result + ", bigger:", arr2.sort(sortArr));
+//        }
+//    }
+//};
+//console.log(compare([7, 2], [7, 2]));
+//console.log(compare([3, 1, 4], [8]));
+//console.log(compare([3, 1, 4], [8, 2]));
+
+
+
+
+//version____________________________3
+
+//var a = [1, 13, 5];
+//var b = [5, 1, 2];
+//    
+//function compare(a, b) {
+//    var bigger = '';
+//    if (Array.isArray(a) && Array.isArray(b)) {                          //массивы ли это
+//        if (a.length == b.length) {                                      //одинаковые по длине?
+//            if (JSON.stringify(a) == JSON.stringify(b)) return true;
+//        } else {
+//            if (summa(a) === summa(b)) {
+//                return '{result:false, bigger:[' + bigger + ']}';
+//            } else if (summa(a) > summa(b)) {
+//                bigger = (a.sort(compareNumeric)).toString();
+//            } else {
+//                bigger = (b.sort(compareNumeric)).toString();
+//            }
+//            return '{result:false, bigger:[' + bigger + ']}';
+//        }
+//    }
+//    return false;
+//}
+//function summa(a) {
+//    var sum = 0;
+//    for (var i = 0; i < a.length; i++) {
+//        sum += parseInt(a[i], 10);
+//    }
+//    return sum;
+//}
+//function compareNumeric(a, b) {
+//    if (a > b) return 1;
+//    if (a < b) return -1;
+//}
+//console.log(compare());
