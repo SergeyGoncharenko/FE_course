49a08c52bf971bc6788e709fd8b0749e290852d0
SergeyGoncharenko <mailgoncharenko@gmail.com>
2015-12-15 21:27:51 +0200

    JavaScript, jQuery
    part1: jQuery-метод .each(), JS Loop - for, доступ к элементам DOM, их
    атрибутам и свойствам.
    Небольшие изменения в исходном HTML и CSS:
    -legend нельзя вставить в div?
    -в .container добавил .row

---
+++ Lesson 7/jQscript.js
@@ -0,0 +1,17 @@
+﻿//jQuery, добавление/изменение атрибута с помощью метода .attr( name, function( [index, attr] ) )
+
+$(document).ready(function () {
+
+    $("input").attr("name", function (i) {
+        ++i; // Увеличим на 1, т.к. отсчет идет с 0
+        return this.id;
+    });
+
+    $('input').each(function (elem) {
+        if ($(this).type == "checkbox" || ($(this).type == "radio")) {
+            return false;
+        } else {
+            return $(elem).text();
+        }
+    });
+})
---
+++ Lesson 7/script.js
@@ -0,0 +1,16 @@
+﻿
+window.onload = function () {
+
+    //js, добавить атрибут name со значением его id
+    function addAttrName() {
+        var inputs = document.getElementsByTagName("input");
+        for (var i = 0; i < inputs.length; i++) {
+            if (inputs[i].type !== "checkbox" || inputs[i].type !== "radio") {
+                var inputId = inputs[i].getAttribute("id");
+                inputs[i].setAttribute("name", inputId);
+            }
+        }
+    }
+    addAttrName();
+
+}//good job!
